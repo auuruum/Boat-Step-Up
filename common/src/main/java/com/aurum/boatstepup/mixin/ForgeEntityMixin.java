643c4dee4709570529b76c28a8f92b58df3cd907
auuruum <aurimasdovgialo@gmail.com>
2026-06-26 02:32:02 +0300

    Add universal jar and split platform mixin configs

--- common/src/main/java/com/aurum/boatstepup/mixin/ForgeEntityMixin.java
+++ common/src/main/java/com/aurum/boatstepup/mixin/ForgeEntityMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
-public abstract class EntityMixin {
+public abstract class ForgeEntityMixin {
     @Inject(method = "maxUpStep", at = @At("HEAD"), cancellable = true)
     private void boatstepup$maxUpStep(CallbackInfoReturnable<Float> cir) {
         if ((Object) this instanceof Boat) {
--- fabric/src/main/java/com/aurum/boatstepup/fabric/mixin/FabricEntityMixin.java
+++ fabric/src/main/java/com/aurum/boatstepup/fabric/mixin/FabricEntityMixin.java
@@ -1,4 +1,4 @@
-package com.aurum.boatstepup.mixin;
+package com.aurum.boatstepup.fabric.mixin;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.vehicle.BoatEntity;
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
-public abstract class EntityMixin {
+public abstract class FabricEntityMixin {
     @Inject(method = "getStepHeight", at = @At("HEAD"), cancellable = true)
     private void boatstepup$getStepHeight(CallbackInfoReturnable<Float> cir) {
         if ((Object) this instanceof BoatEntity) {
