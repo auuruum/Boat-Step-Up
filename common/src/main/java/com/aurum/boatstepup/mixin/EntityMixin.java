78e0b1cea125e1cb9cf400ee0daad3407d3a10ff
auuruum <aurimasdovgialo@gmail.com>
2026-06-26 02:16:41 +0300

    Migrate to multi-loader Gradle setup for 1.21.1

---
+++ common/src/main/java/com/aurum/boatstepup/mixin/EntityMixin.java
@@ -0,0 +1,18 @@
+package com.aurum.boatstepup.mixin;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.vehicle.Boat;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+    @Inject(method = "maxUpStep", at = @At("HEAD"), cancellable = true)
+    private void boatstepup$maxUpStep(CallbackInfoReturnable<Float> cir) {
+        if ((Object) this instanceof Boat) {
+            cir.setReturnValue(1.0F);
+        }
+    }
+}
---
+++ fabric/src/main/java/com/aurum/boatstepup/mixin/EntityMixin.java
@@ -0,0 +1,18 @@
+package com.aurum.boatstepup.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.vehicle.BoatEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+    @Inject(method = "getStepHeight", at = @At("HEAD"), cancellable = true)
+    private void boatstepup$getStepHeight(CallbackInfoReturnable<Float> cir) {
+        if ((Object) this instanceof BoatEntity) {
+            cir.setReturnValue(1.0F);
+        }
+    }
+}
